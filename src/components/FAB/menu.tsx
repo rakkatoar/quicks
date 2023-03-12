8308d3b67c39dd45f56345456534c44b7b686cb2
Rakka Toar <rakkatoar@gmail.com>
2023-03-12 23:12:59 +0700

    fixed close menu button

--- src/components/FAB/menu.tsx
+++ src/components/FAB/menu.tsx
@@ -18,7 +18,7 @@ export const MenuButton = (props: IPopUp) => {
     <>
 			{menus.map((menu, index) =>
 			<div key={menu.title} className='z-20'>
-				<button className={`w-[40px] h-[40px] bottom-3 rounded-full duration-500 ${menu.active ? 'absolute bg-[#4F4F4F] right-[22px]' : 'right-3 hidden'}`} onClick={() => changeActiveMenu(-1)} ></button>
+				<button className={`w-[40px] h-[40px] bottom-3 rounded-full duration-500 ${menu.active ? 'fixed bg-[#4F4F4F] right-[22px]' : 'right-3 hidden'}`} onClick={() => changeActiveMenu(-1)} ></button>
 				<button key={menu.title} className={`right-3 fixed bottom-3 duration-500`} style={{ marginRight : showMenu && !isPopUpActive ? (index+1)*50+'px' : showMenu && isPopUpActive ? (index)*60+'px' : '0'}} onClick={() => changeActiveMenu(index)}>
 					<p className={`${showMenu && !isPopUpActive ? 'opacity-100 -top-6' : 'opacity-0 top-0'} w-full mx-auto absolute text-sm text-[#F2F2F2] font-semibold mb-1 delay-100 duration-100`}>{menu.title}</p>
 					<div className={`w-[40px] h-[40px] rounded-full`} style={{backgroundColor : menu.active ? menu.color : '#F2F2F2'}}>
